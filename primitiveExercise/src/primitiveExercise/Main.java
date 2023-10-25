7ee8d53660506e5df47ebf85eaf575777ec2e809
Ben Veilleux <147216774+baveilleux@users.noreply.github.com>
2023-10-24 20:20:59 -0600

    Through 2.5
    Concatenating strings in Java

---
+++ primitiveExercise/src/primitiveExercise/Main.java
@@ -0,0 +1,18 @@
+package primitiveExercise;
+
+public class Main {
+	public static void main(String[] args) {
+		int studentAge = 15;
+		double studentGPA = 3.45;
+		char studentFirstInit = 'K';
+		//charAt Operation indexes Strings starting at 0
+		//char studentFirstInit = studentFirstName.charAt(0);
+		char studentLastInit = 'H';
+		//strings have double quotes
+		String studentName = "Ben Veilleux";
+		boolean perfectAttendance = true;
+		
+		System.out.println(studentName + " is " + studentAge + " years old and has a GPA of " + studentGPA + ".");
+		
+	}
+}
